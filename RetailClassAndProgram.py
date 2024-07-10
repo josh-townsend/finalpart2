3db79e06b793db5329c6a255a0f6f32ef123f2f3
Josh Townsend <josh_townsend1@baylor.edu>
2024-07-09 20:45:33 -0500

    gitignore

---
+++ RetailClassAndProgram.py
@@ -0,0 +1,52 @@
+# This program creates the class file and the program file together
+# NOTE: wherever you see 'xxxx', that means there is code missing and
+# is to be completed by you.
+
+
+#Class definition (part 1) 
+
+class RetailItem():
+    def __init__(self,xxxxxxxxxxxxxxxxx):
+        self.__desc = xxxxxxxx
+        xxxxxx
+        xxxxxx
+
+    def get_desc(self):
+        xxxxx
+
+    def set_desc(self, desc):
+        xxxx
+        xxxx
+
+    xxxx
+    xxxx
+    xxxx
+    xxxx
+
+
+
+
+
+
+
+
+# program (part 2)
+
+# create instances
+item1 = xxxxx
+item2 = xxxxx
+item3 = xxxxx
+
+
+
+# print out desc and price
+
+xxxx
+xxxx
+xxxx
+
+        
+
+
+
+    
